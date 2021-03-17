6792a607d7162041c010708f2929731131a41038
szymel2020 <szymon.chala@gmail.com>
2021-03-17 22:41:21 +0100

    first exercise in game

--- game.js
+++ game.js
@@ -1,65 +1,66 @@
 //tworzenie funkcji anonimowej przypisanej do zmiennej 'wybieranie'
+
 const wybieranie = function () {
 	// utworzenie tablicy
-	var tablica = [ 'C', 'Y', 'B', ];
+	var tablica = ['C', 'Y', 'B', ];
 	var tgu = document.getElementById("numer").innerHTML = tablica[Math.random() * tablica.length << 0];
 	if (tgu == 'B') {
 		document.getElementById('numer').style.backgroundColor = 'red';
 		document.getElementById('numer').style.color = 'white';
 		//dodanie elementu 'div' do body
-	} else if(tgu == 'C') {
+	} else if (tgu == 'C') {
 		document.getElementById('numer').style.backgroundColor = 'yellow';
 		document.getElementById('numer').style.color = 'black';
-	} else if(tgu == 'Y') {
+	} else if (tgu == 'Y') {
 		document.getElementById('numer').style.backgroundColor = 'black';
 		document.getElementById('numer').style.color = 'orange';
-};
+	};
 }
 const kulka = document.querySelector("#numer");
-kulka.addEventListener("click", wybieranie);
+kulka.addEventListener("click", wybieranie, bingo);
+
+// funkcja losowania litery dla 'kulki1'
 
 const wybieranie1 = function () {
-	//utworzenie elementu 'div'
 
 	var tablica = ['A', 'E', 'O'];
 	var tgu = document.getElementById("numer1").innerHTML = tablica[Math.random() * tablica.length << 0];
-	if(tgu == 'O') {
+	if (tgu == 'O') {
 		document.getElementById('numer1').style.backgroundColor = 'red';
 		document.getElementById('numer1').style.color = 'white';
-	}else if(tgu == 'A') {
+	} else if (tgu == 'A') {
 		document.getElementById('numer1').style.backgroundColor = 'yellow';
 		document.getElementById('numer1').style.color = 'black';
 	} else if (tgu == 'E') {
 		document.getElementById('numer1').style.backgroundColor = 'black';
 		document.getElementById('numer1').style.color = 'orange';
 	};
-	//dodanie elementu 'div' do body
 
 }
 
 const kulka1 = document.querySelector("#numer1");
-kulka1.addEventListener("click", wybieranie1);
+kulka1.addEventListener("click", wybieranie1, bingo);
 
+
+// funkcja losowania litery dla 'kulki2'
 const wybieranie2 = function () {
-	//utworzenie elementu 'div'
 
 	var tablica = ['X', 'S', 'T'];
 	var tgu = document.getElementById("numer2").innerHTML = tablica[Math.random() * tablica.length << 0];
-	if(tgu == 'X') {
+	if (tgu == 'X') {
 		document.getElementById('numer2').style.backgroundColor = 'red';
 		document.getElementById('numer2').style.color = 'white';
-	}else if(tgu == 'T') {
+	} else if (tgu == 'T') {
 		document.getElementById('numer2').style.backgroundColor = 'yellow';
 		document.getElementById('numer2').style.color = 'black';
-	}else if(tgu == 'S') {
+	} else if (tgu == 'S') {
 		document.getElementById('numer2').style.backgroundColor = 'black';
 		document.getElementById('numer2').style.color = 'orange';
 	};
-	//dodanie elementu 'div' do body
 
 }
 const kulka2 = document.querySelector("#numer2");
-kulka2.addEventListener("click", wybieranie2);
+kulka2.addEventListener("click", wybieranie2, bingo);
 
 
 
@@ -88,6 +89,13 @@ button.addEventListener("click", function () {
 	textbox.value = "";
 });
 // funkcja wygrania losowania
+var bingo = function () {
+	if(numer.style.backgroundColor = "red") {
+		document.getElementById('name').innerHTML = 'win';
+	};
+};
+// funkcja wygrania losowania
+
 
 
 

821c585658547e8f59af4fdd285ce8ee5aa89966
szymel2020 <szymon.chala@gmail.com>
2021-03-29 23:08:30 +0200

    create day of week

--- game.js
+++ game.js
@@ -1,4 +1,30 @@
 
+// Day of week
+
+	var day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
+document.getElementById("demo").innerHTML =  day;
 
 
 //tworzenie funkcji anonimowej przypisanej do zmiennej 'wybieranie'
